5298517d8e0a548c138a97487fb2cc4c9d7e43dc
Ahmed Ayman <69440618+AhmedAymanBfcai@users.noreply.github.com>
2021-09-27 00:03:42 +0200

    Try to solve deployment problems

--- app.js
+++ app.js
@@ -1,15 +1,20 @@
 const express = require('express')
 const app = express()
-const port = 3003
 const middleware = require('./middleware')
 const path = require('path')
 const bodyParser = require('body-parser')
 const mongoose = require('./database')
-const session = require('express-session')
+// const session = require('express-session')
+const session = require('cookie-session')
 
-const server = app.listen(port, () =>
-  console.log('Server listening on port ' + port)
+const dotenv = require('dotenv')
+dotenv.config()
+
+const PORT = process.env.PORT || 3003
+const server = app.listen(PORT, () =>
+  console.log('Server listening on port ' + PORT)
 )
+
 const io = require('socket.io')(server, { pingTimeout: 60000 })
 
 app.set('view engine', 'pug')
@@ -17,6 +22,14 @@ app.set('views', 'views')
 
 app.use(bodyParser.urlencoded({ extended: false }))
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use(
+  session({
+    secret: 'bbq chips',
+    resave: true,
+    saveUninitialized: false,
+  })
+)
 
 app.use(
   session({
